001e23b524424c67b7ac4103e80338a6a9484874
kilbergr <rekilberg@gmail.com>
2015-07-13 22:18:40 -0700

    with styling

--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -2,6 +2,5 @@ class Puppy < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true
 	validates :age, presence: true
 	validates :breed, presence: true
-	validates :image, presence: true
 	validates :bio, presence: true
 end
